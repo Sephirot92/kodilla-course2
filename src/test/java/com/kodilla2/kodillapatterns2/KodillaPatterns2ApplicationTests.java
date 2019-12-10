7e543f79799c3cfdf75a25ef80385faabcc7e81b
Łukasz Czapraga <lczapraga@gmail.com>
2019-12-10 21:27:55 +0100

    Module 24.1 - First commit.

---
+++ src/test/java/com/kodilla2/kodillapatterns2/KodillaPatterns2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla2.kodillapatterns2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaPatterns2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
